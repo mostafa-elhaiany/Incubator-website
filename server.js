1cbc68ba446042e9299ac1c827403b62a1cf4092
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2019-09-22 11:04:05 +0200

    edited server.js

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const app=express()
 app.use(bodyParser.json())
 
 
+const db= require('./config/keys').mongoURI
+
+mongoose.connect(db, { useNewUrlParser: true,useUnifiedTopology: true })
+    .then(()=>console.log('connected to mongo db'))
+    .catch(err=> console.log(err))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>console.log(`server running on port ${port}`))
