abbbfb1dcaf1f9211cb323f00288d7935d21f150
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2019-09-29 16:56:25 +0200

    added team tree in about, and added organizing director

--- client/src/views/LandingPage/Sections/TeamSection.js
+++ client/src/views/LandingPage/Sections/TeamSection.js
@@ -30,6 +30,51 @@ export default function TeamSection() {
       <h2 className={classes.title}>Here is our team</h2>
       <div>
         <GridContainer>
+        <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img 
+                  // src={team1} 
+                  alt="..." className={imageClasses} />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                Yassmin Sobhy
+                <br />
+                <small className={classes.smallTitle}>Organizing Director</small>
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                  You can write here details about one of your team members. You
+                  can give more details about what they do. Feel free to add
+                  some <a href="#pablo">links</a> for people to be able to
+                  follow them outside the site.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-twitter"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-instagram"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-facebook"} />
+                </Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <Card plain>
               <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
@@ -40,7 +85,7 @@ export default function TeamSection() {
               <h4 className={classes.cardTitle}>
                 Samar Abdelaty
                 <br />
-                <small className={classes.smallTitle}>Head</small>
+                <small className={classes.smallTitle}>President</small>
               </h4>
               <CardBody>
                 <p className={classes.description}>
